afb522361f9770ff88094167358a5fb6d72e7066
Bruno Bieth <biethb@gmail.com>
2016-01-12 17:24:16 +0100

    CatDemo cosmetic

--- demo/src/main/scala/CatDemo.scala
+++ demo/src/main/scala/CatDemo.scala
@@ -6,17 +6,15 @@ object CatDemo {
 
   class Cat extends Command(description = "concatenate files and print on the standard output") {
 
-    var files = args[Seq[File]](description = "files to concat")
-
-    var showAll = opt[Boolean](abbrev = "A", description = "equivalent to -vET")
+    var showAll        = opt[Boolean](abbrev = "A", description = "equivalent to -vET")
     var numberNonblank = opt[Boolean](abbrev = "b", description = "number nonempty output lines, overrides -n")
+
+    var files          = args[Seq[File]](description = "files to concat")
   }
 
   def main(args: Array[String]) {
-    Cli.parse(args)
-      .version("1.2")
-      .withCommand(new Cat) { case cat =>
-          println(cat.files)
-      }
+    Cli.parse(args).withCommand(new Cat) { case cat =>
+        println(cat.files)
+    }
   }
 }
