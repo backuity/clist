b39be76b3385a40023a0889561faff4d2625f7ed
Bruno Bieth <biethb@gmail.com>
2016-06-10 14:06:15 +0200

    add a convenience method for testing CliMain

--- core/src/main/scala/org/backuity/clist/CliMain.scala
+++ core/src/main/scala/org/backuity/clist/CliMain.scala
@@ -3,8 +3,16 @@ package org.backuity.clist
 abstract class CliMain(name: String = null, description: String = "") extends Command(name, description) {
 
   final def main(args: Array[String]): Unit = {
+    runWith(args)
+  }
+
+  /**
+    * Convenience method for testing the CLI.
+    * @return None if `args` are invalid or if the usage/help was requested
+    */
+  def runWith[T](args: Array[String]): Option[T] = {
     Cli.parse(args).withCommand(this) { _ => run }
   }
 
-  def run: Unit
+  def run[T]: T
 }
